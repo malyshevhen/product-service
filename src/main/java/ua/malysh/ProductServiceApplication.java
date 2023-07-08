5d8d90dd3a12101e4cabbe2cb56b740a881e5fd2
Evhen <malysh.evgeniy@gmail.com>
2023-07-08 15:58:34 +0300

    Remove duplicated annotation

--- src/main/java/ua/malysh/ProductServiceApplication.java
+++ src/main/java/ua/malysh/ProductServiceApplication.java
@@ -2,10 +2,8 @@ package ua.malysh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
-@EnableMethodSecurity
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
