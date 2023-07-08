3c91803c6b9fb289e9bee9c7e2c5922fea7615d3
Evhen <malysh.evgeniy@gmail.com>
2023-07-08 15:59:28 +0300

    refactor code

--- src/main/java/ua/malysh/jwt/JwtRoleConverter.java
+++ src/main/java/ua/malysh/jwt/JwtRoleConverter.java
@@ -1,25 +1,25 @@
 package ua.malysh.jwt;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
 public class JwtRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
 
     @Override
-    public Collection convert(Jwt jwt) {
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
 
         @SuppressWarnings("unchecked")
-        var roles = (ArrayList<String>) jwt.getClaims().get("roles");
+        final var roles = (ArrayList<String>) jwt.getClaims().get("roles");
 
-        if (roles == null || roles.isEmpty()) {
-            return new ArrayList<>();
-        }
-
-        return roles.stream().map(SimpleGrantedAuthority::new).toList();
+        return roles
+                .stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
     }
 }
